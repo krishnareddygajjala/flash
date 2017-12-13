03a5bb8b4217fd8e11a62567b6b56f6a13c3daee
krishnareddygajjala <gvkreddy71@gmail.com>
2017-12-13 10:45:01 +0530

    this is second project

---
+++ Practice/python/String.py
@@ -0,0 +1,13 @@
+s = "   i am krishna   "
+print s.capitalize()
+print s.upper()
+print s.lower()
+print s.title()
+print s.strip()
+if s.islower() == True:
+    print "string is lower"
+
+print s.center(40)
+print s.index("i")
+print s.count("a")
+print s.replace("am", "am not")
